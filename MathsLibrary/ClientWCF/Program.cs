4cd541ad9c829cda640fb0d2a9728fce2204448f
OsgartOlivia <olivia.osgart@etu.univ-cotedazur.fr>
2019-04-23 23:15:34 +0200

    TD2 SOAP

---
+++ MathsLibrary/ClientWCF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClientWCF.ServiceConnected;
+
+namespace ClientWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperationsClient client = new MathsOperationsClient(); /* classe d’accès au service */
+            Console.WriteLine(client.Add(100, 101));
+            Console.WriteLine(client.Multiply(100, 101));
+            Console.ReadLine();
+        }
+    }
+}
