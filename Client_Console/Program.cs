bbfcfc19290603e83f261e44eccdd1882fa804db
Olivia Osgart <olivia.osgart@etu.univ-cotedazur.fr>
2019-05-12 09:49:42 +0200

    Connected the client to IntermediaryService

--- Client_Console/Program.cs
+++ Client_Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Client_Console.VelibSOAP;
 
 namespace Client_Console
 {
@@ -10,6 +11,43 @@ namespace Client_Console
     {
         static void Main(string[] args)
         {
+            IntermediaryServiceClient serviceClient = new IntermediaryServiceClient();
+
+            Console.WriteLine("Bienvenue sur la version console de Velib WS");
+
+            Console.WriteLine("---Commandes disponibles---");
+            Console.WriteLine("villes --- liste des villes disponibles");
+            Console.WriteLine("stations --- liste de toutes les stations");
+            Console.WriteLine("stations <ville> --- liste des stations d'une ville");
+            Console.WriteLine("velibs <station> --- nombre de velibs sur une station");
+            Console.WriteLine("velibs <ville> --- nombre de velibs sur une ville");
+            Console.WriteLine("position <station> --- position d'une station");
+            Console.WriteLine("positions <ville> --- position des stations d'une ville");
+            Console.WriteLine("quitter --- quitter l'application");
+
+            string input = "";
+
+            while (true)
+            {
+                Console.Write(">");
+
+                input = Console.ReadLine();
+                string[] argsT = input.Split(' ');
+                if (argsT.Length < 1)
+                {
+                    continue;
+                }
+
+                if (argsT[0] == "quit")
+                {
+                    break;
+                }
+                else if (argsT[0] == "stations")
+                {
+                    string station = serviceClient.GetStations();
+                    Console.WriteLine(station);
+                }
+            }
         }
     }
 }
